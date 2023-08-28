688cc878e87a1f24120f78ba02577c0b361dbd92
floof <fl0_0d@mail.ru>
2023-08-28 09:04:57 +0300

    доработка

--- src/employeer/router.py
+++ src/employeer/router.py
@@ -11,28 +11,28 @@ employee_router = APIRouter(prefix="/employeer", tags=["employeer"])
 @employee_router.get("/", response_model=list[EmployeeStructures],
                      status_code=status.HTTP_200_OK)
 async def get_all_employees(session: AsyncSession = Depends(get_async_session)):
-    return service.get_all_employees(session)
+    return await service.get_all_employees(session)
 
 
 @employee_router.get("/{employee_id}", response_model=EmployeeStructures,
                      status_code=status.HTTP_200_OK)
 async def get_employee(employee_id: int, session: AsyncSession = Depends(get_async_session)):
-    return service.get_employee_by_id(session, employee_id)
+    return await service.get_employee_by_id(session, employee_id)
 
 
 @employee_router.post("/", response_model=EmployeeStructures,
                       status_code=status.HTTP_201_CREATED)
 async def create_employee(employee: EmployeeStructures,
                           session: AsyncSession = Depends(get_async_session)):
-    return service.create_employee(session, employee)
+    return await service.create_employee(session, employee)
 
 
 @employee_router.put("/{employee_id}", response_model=EmployeeStructures)
 async def update_employee(employee_id: int, data: EmployeeStructures,
                           session: AsyncSession = Depends(get_async_session)):
-    return service.update_employee(session, employee_id, data)
+    return await service.update_employee(session, employee_id, data)
 
 
 @employee_router.delete("/{employee_id}")
 async def delete_employee(employee_id: int, session: AsyncSession = Depends(get_async_session)):
-    return service.delete_employee(session, employee_id)
+    return await service.delete_employee(session, employee_id)
--- src/task/router.py
+++ src/task/router.py
@@ -10,24 +10,24 @@ tasks_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 @tasks_router.get("/", response_model=list[TaskStructures], status_code=status.HTTP_200_OK)
 async def get_all_tasks(session: AsyncSession = Depends(get_async_session)):
-    return service.get_all_tasks(session)
+    return await service.get_all_tasks(session)
 
 
 @tasks_router.post("/", response_model=TaskStructures, status_code=status.HTTP_201_CREATED)
 async def create_task(task: TaskCreateStructures, session: AsyncSession = Depends(get_async_session)):
-    return service.create_task(session, task)
+    return await service.create_task(session, task)
 
 
 @tasks_router.get("/{task_id}", response_model=TaskStructures, status_code=status.HTTP_200_OK)
 async def get_task(task_id: int, session: AsyncSession = Depends(get_async_session)):
-    return service.get_task_by_id(session, task_id)
+    return await service.get_task_by_id(session, task_id)
 
 
 @tasks_router.delete("/{task_id}")
 async def delete_task(task_id: int, session: AsyncSession = Depends(get_async_session)):
-    return service.delete_task(session, task_id)
+    return await service.delete_task(session, task_id)
 
 
 @tasks_router.put("/{task_id}", response_model=TaskStructures, status_code=status.HTTP_200_OK)
 async def update_task(task_id: int, data: TaskCreateStructures, session: AsyncSession = Depends(get_async_session)):
-    return service.update_task(session, task_id, data)
+    return await service.update_task(session, task_id, data)
